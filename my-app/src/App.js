2728a0e8b8e616698a7263bb5bb0ce3669ded1f1
Mobilpymes <whuera@gmail.com>
2021-01-19 00:25:52 -0500

    add primefaces

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import { Component, useEffect, useState } from "react";
 import { UserService } from "./service/UserService";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { Panel } from 'primereact/panel';
-import { Card } from 'primereact/card';
+import { Panel } from "primereact/panel";
+import { Card } from "primereact/card";
 
-import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
+import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+
+
 
 export default class App extends Component {
   constructor() {
@@ -22,15 +24,15 @@ export default class App extends Component {
   }
   render() {
     return (
-      <Panel header="Header" toggleable>
-      <Card title="Call API" subTitle="Data From Microservices" >
-        <DataTable value={this.state.user}>
-          <Column field="id" header="ID"></Column>
-          <Column field="firstName" header="Nombres"></Column>
-          <Column field="lastName" header="Apellidos"></Column>
-        </DataTable>
-      </Card>
-      </Panel>
+      <div>
+        <div className="card">
+          <DataTable value={this.state.user} header="Header" footer="Footer" className="p-datatable-gridlines">
+            <Column field="id" header="ID"></Column>
+            <Column field="firstName" header="Nombres"></Column>
+            <Column field="lastName" header="Apellidos"></Column>
+          </DataTable>
+        </div>
+      </div>
     );
   }
 }
