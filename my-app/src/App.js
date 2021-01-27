93d4cf09188cb1aef02fc075ae9798c58a6917c0
Mobilpymes <whuera@gmail.com>
2021-01-27 18:04:11 -0500

    formating

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
-  const url = "http://localhost:8080/users"
-  const [users, setUsers] = useState()
-  const fetchApi = async() => {
-    const response = await fetch(url)
-    console.log(response.status)
-    const responseJSON = await response.json()
-    setUsers(responseJSON)
-    console.log(responseJSON)
-  }
-  useEffect(() => {
-    fetchApi()
-  },[])
-  return (
-    <div className="App">
-      Hola Mundo 2!!
-      <ul>
-        {!users ? 'Cargando....' : 
-        users.map((user, index)=>{
-          return <li>{user.firstName}</li>
-        }
-
-        )
-        }
-      </ul>
-    </div>
-  );
+    const url = "http://localhost:8080/users"
+    const [users, setUsers] = useState()
+    const fetchApi = async () => {
+        const response = await fetch(url)
+        console.log(response.status)
+        const responseJSON = await response.json()
+        setUsers(responseJSON)
+        console.log(responseJSON)
+    }
+    useEffect(() => {
+        fetchApi()
+    }, [])
+    return (
+        <div className="App">
+            Hola Mundo 2!!
+            <ul>
+                {!users ? 'Cargando....' :
+                    users.map((user, index) => {
+                            return <li>{user.firstName}</li>
+                        }
+                    )
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
